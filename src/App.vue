dc87e74b705a51a15b31a4854fa4a7bcdefb8c3f
bsquiroz <bemq1605@gmail.com>
2024-04-28 19:52:06 -0500

    project base

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Layout from "./components/Layout/Layout.vue";
+</script>
+
+<template>
+  <Layout>
+    <h2 class="color--primary">Hola mundo</h2>
+  </Layout>
+</template>
