71d8244db2fac67044a575764e368717c63554f2
bsquiroz <bemq1605@gmail.com>
2024-04-28 20:22:49 -0500

    add section banner

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Layout from "./components/Layout/Layout.vue";
+import Banner from "./components/Banner/Banner.vue";
 </script>
 
 <template>
+  <Banner />
   <Layout>
     <h2 class="color--primary">Hola mundo</h2>
   </Layout>
---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section
+    class="relative h-[60vh] bg-[url('/images/banner2.jfif')] bg-no-repeat bg-cover bg-center bg-fixed overflow-hidden"
+  >
+    <div class="absolute inset-0 bg-black/70"></div>
+
+    <section
+      class="text-white relative max-w-5xl m-auto p-5 min-h-screen pt-20"
+    >
+      <div class="flex flex-col gap-5">
+        <a class="font-bold text-2xl" href="#"
+          >kEYBOARD <span class="color--primary">BS</span></a
+        >
+
+        <p>
+          ¡Bienvenido a Keyboardbs, tu destino para teclados mecánicos de
+          calidad a precios inigualables! En Keyboardbs, nos enorgullece
+          ofrecerte una amplia selección de teclados diseñados pensando en tu
+          salud, tu bolsillo y tu ergonomía. Desde opciones asequibles hasta lo
+          más exclusivo, tenemos el teclado perfecto para ti.
+        </p>
+
+        <p>
+          Explora nuestra colección que abarca desde teclados fabricados en
+          madera hasta los más lujosos en mármol.
+        </p>
+
+        <p>
+          Nuestros materiales de primera calidad no solo brindan una estética
+          excepcional, sino que también garantizan durabilidad y un rendimiento
+          óptimo.
+        </p>
+
+        <button class="bg--primary p-5 self-center rounded-md font-bold">
+          Echales un ojo
+        </button>
+
+        <div class="self-center">
+          <i
+            class="bx bx-down-arrow-alt bx-fade-down-hover text-5xl cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<style scoped></style>
